3edf4783f2afabd7532572d8134ddc6741048c90
Luis-Sy <74744192+Luis-Sy@users.noreply.github.com>
2025-04-06 14:54:54 -0600

    edit app.js file and add test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TECH2102 - Enterprise Computing</h1>
+      <h1>Group 14</h1>
+	  <div>
+		<h2>Felipe Alves Barbosa</h2>
+		<h2>Gabriel Allen Anareta</h2>
+		<h2>Jaryd Pohorelic</h2>
+		<h2>Luis Marlou Sy</h2>
+	  </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const e1 = screen.getByText(/Felipe Alves Barbosa/i);
+  const e2 = screen.getByText(/Gabriel Allen Anareta/i);
+  const e3 = screen.getByText(/Jaryd Pohorelic/i);
+  const e4 = screen.getByText(/Luis Marlou Sy/i);
+  expect(e1).toBeInTheDocument();
+  expect(e2).toBeInTheDocument();
+  expect(e3).toBeInTheDocument();
+  expect(e4).toBeInTheDocument();
 });
